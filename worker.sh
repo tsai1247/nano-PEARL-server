d58e245a95c26481bacfc3fff3caeb051c3a9e6f
nctu6 <nctu6.tw@gmail.com>
2025-12-17 08:58:37 +0800

    add server scripts

---
+++ worker.sh
@@ -0,0 +1,3 @@
+python3 -m fastchat.serve.pearl_worker --model-path /home/ubuntu/models/Qwen/Qwen3-32B --draft-model-path /home/ubuntu/models/Qwen/Qwen3-1.7B --model-name test --controller http://localhost:21001 --port 31000 --worker http://localhost:31000
+
+
